62e88e47c6fafb5c539b690ce5b5d28903ea78fe
Raza <rtshareef@gmail.com>
2020-09-28 15:20:39 -0400

    day1

---
+++ swift/2sum.swift
@@ -0,0 +1,15 @@
+// ! Day 1 // 9.28
+
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var map: [Int:Int] = [:]
+        for i in 0 ..< nums.count {
+            if let mapped = map[target - nums[i]] {
+                return [mapped,i]
+            }
+            map[nums[i]] = i 
+        }
+        return []
+    }
+}
