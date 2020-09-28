784fbe477ee73a184482bbea3166bb44d19a6c36
Raza <rtshareef@gmail.com>
2020-09-28 17:09:09 -0400

    correct

--- js/0001-two-sum.js
+++ js/0001-two-sum.js
@@ -3,10 +3,11 @@
 var twoSum = function(nums, target) {
   const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    let currVal = num[i];
+    let currVal = nums[i];
     if (map.has(currVal)) {
       return [map.get(currVal), i];
     }
+
     let diff = target - currVal;
     map.set(diff, i);
   }
