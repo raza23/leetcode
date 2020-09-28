792054ff094fa565d5d0410f2f3e5b85bf053e84
Raza <rtshareef@gmail.com>
2020-09-28 14:30:45 -0400

    2 sum

---
+++ js/twosum.js
@@ -0,0 +1,13 @@
+// ! Day 1 // 9.28
+
+var twoSum = function(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let currVal = num[i];
+    if (map.has(currVal)) {
+      return [map.get(currVal), i];
+    }
+    let diff = target - currVal;
+    map.set(diff, i);
+  }
+};
