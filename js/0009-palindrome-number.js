b963c499efa0e81f65865c4301d373f93bbeab87
Raza <rtshareef@gmail.com>
2020-09-29 16:14:30 -0400

    completed palindrome number in JS

--- js/0009-palindrome-number.js
+++ js/0009-palindrome-number.js
@@ -1,4 +1,16 @@
 // ! Day 2
 // ! 9.28
 
-var isPalindrome = function(x) {};
+var isPalindrome = function(x) {
+  if (x < 0) return false;
+  return x === reversedNum(x);
+};
+
+var reversedNum = function(x) {
+  let reverse = 0;
+  while (x > 0) {
+    reverse = reverse * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+  return reverse;
+};
