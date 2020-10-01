6f26e9505a1f6cf63eaeac05ebab45e11bc71ac4
Raza <rtshareef@gmail.com>
2020-10-01 15:18:18 -0400

    completed happynumber.js

---
+++ javascript/0202-happy-number.js
@@ -0,0 +1,20 @@
+// Day 4
+// 10.1
+
+var isHappy = function(n) {
+  const previous = new Set();
+  while (n !== 1) {
+    let currentSum = 0;
+    while (n > 0) {
+      currentSum += (n % 10) * (n % 10);
+      n = Math.floor(n / 10);
+    }
+    if (!previous.has(currentSum)) {
+      previous.add(currentSum);
+      n = currentSum;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
