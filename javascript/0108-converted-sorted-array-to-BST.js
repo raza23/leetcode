1da3da27656c93903d3db8b583193d3341fe0fd2
Raza <rtshareef@gmail.com>
2020-10-20 13:49:35 -0400

    convert sorted array to bst done

---
+++ javascript/0108-converted-sorted-array-to-BST.js
@@ -0,0 +1,16 @@
+// 10/20
+
+var sortedArrayToBST = function(nums) {
+  return traverse(nums, 0, nums.length - 1);
+};
+
+function traverse(nums, start, end) {
+  if (start > end) {
+    return null;
+  }
+  let mid = Math.floor((start + end) / 2);
+  let root = new TreeNode(nums[mid]);
+  root.left = traverse(nums, start, mid - 1);
+  root.right = traverse(nums, mid + 1, end);
+  return root;
+}
