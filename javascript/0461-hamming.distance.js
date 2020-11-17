3b58a563fbfd4602f940330135ba8b0a10929685
Raza <rtshareef@gmail.com>
2020-11-17 12:54:55 -0500

    hamming distance done

---
+++ javascript/0461-hamming.distance.js
@@ -0,0 +1,19 @@
+// 11.17
+
+var hammingDistance = function(x, y) {
+  let result = 0;
+
+  while (x > 0 || y > 0) {
+    const right1 = x % 2;
+    const right2 = y % 2;
+
+    if (right1 !== right2) {
+      result += 1;
+    }
+
+    x = Math.floor(x / 2);
+    y = Math.floor(y / 2);
+  }
+
+  return result;
+};
