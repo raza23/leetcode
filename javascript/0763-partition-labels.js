e3cfb4fcde646781a0c438062af2eb29a4198dd7
Raza <rtshareef@gmail.com>
2021-04-22 08:46:39 -0400

    partition labels done

---
+++ javascript/0763-partition-labels.js
@@ -0,0 +1,30 @@
+var partitionLabels = function(s) {
+  let output = [];
+  let dict = {};
+  let i = 0;
+
+  for (let j = 0; j < s.length; j++) {
+    dict[s[j]] = j;
+  }
+
+  while (i < s.length) {
+    let last = dict[s[i]];
+
+    let k = i;
+
+    while (k < last) {
+      let nextLast = dict[s[k]];
+
+      if (nextLast > last) {
+        last = nextLast;
+      }
+
+      k++;
+    }
+
+    output.push(k - i + 1);
+
+    i = k + 1;
+  }
+  return output;
+};
