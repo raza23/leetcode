667aa8e050bfb7e8e2516969dd95887eb5396813
Raza <rtshareef@gmail.com>
2021-04-28 07:52:15 -0400

    reveal cards in inc order done

---
+++ javascript/0950-reveal-cards-in-inc-order.js
@@ -0,0 +1,14 @@
+var deckRevealedIncreasing = function(deck) {
+  deck.sort((a, b) => a - b);
+
+  var res = [];
+
+  while (deck.length) {
+    if (res.length > 0) {
+      res.unshift(res.pop());
+    }
+
+    res.unshift(deck.pop());
+  }
+  return res;
+};
