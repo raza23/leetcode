93626cf5386f65e73621e9fc7774fa23acab21e3
Raza <rtshareef@gmail.com>
2021-06-28 12:04:25 -0400

    2 sum completed

---
+++ python/2sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m], i]
+                print(d)
+            else:
+                d[n] = i
+                print(d)
