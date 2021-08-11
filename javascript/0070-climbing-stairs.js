a5f3ff8d8c2dbdf9a2e0542972804134f46d8b5c
Raza <rtshareef@gmail.com>
2021-08-11 11:34:42 -0400

    completed counting stairs V2

--- javascript/0070-climbing-stairs.js
+++ javascript/0070-climbing-stairs.js
@@ -14,3 +14,13 @@ var climbStairs = function(n) {
 
   return countingFunc(n, {});
 };
+
+var climbStairs = function(n) {
+  let arr = [1, 2, 3];
+
+  for (let i = 3; i < n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+
+  return arr(n - 1);
+};
