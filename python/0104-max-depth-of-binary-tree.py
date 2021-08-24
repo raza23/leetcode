98556da52555a28bbf9782a3acc4243fb10ddc75
Raza <rtshareef@gmail.com>
2021-08-24 11:03:04 -0400

    max depth of binary tree PYTHON done

---
+++ python/0104-max-depth-of-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxDepth(self, root):
+
+        if root == None:
+            return 0
+
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
